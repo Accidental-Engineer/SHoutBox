81edd12b0c935bb0009eee2e3eb2d1269350fd37
Tarun Kumar <tarun12.tarunkr@gmail.com>
2017-01-28 17:31:13 +0530

    font-awesome

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cooming Soon</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <div id="new">Comming Soon....</div>
+    <a href="index.php" id="an">Go To SHoutBox</a>
+
+  </body>
+</html>
