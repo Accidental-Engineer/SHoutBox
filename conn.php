70ae5c00374ba5bf4484486eeecae85dca56b4a3
Tarun Kumar <tarun12.tarunkr@gmail.com>
2018-02-07 23:49:24 +0530

    s

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $conn_error='Error Occured';
-$conn=mysqli_connect('localhost','root','','shoutit');
-$connect = new mysqli('localhost', 'root', '', 'shoutit');
+$conn=mysqli_connect('localhost','root','7277933031','shoutit');
+$connect = new mysqli('localhost', 'root', '7277933031', 'shoutit');
 if(!@$conn ){
   die($conn_error);
 }
